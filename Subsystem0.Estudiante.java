a083f3e8f789cb7e3c5a8fd09a021d3c5420cb4e
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-27 00:10:08 -0500

    Add files via upload

---
+++ Subsystem0.Estudiante.java
@@ -0,0 +1,17 @@
+package Subsystem0;
+
+public class Estudiante extends Subsystem0.Persona {
+
+	private char colegio de bachillerato;
+
+	private Subsystem0.date fecha de ingreso a la universidad;
+
+	public void abtener() {
+
+	}
+
+	public void guardar() {
+
+	}
+
+}
