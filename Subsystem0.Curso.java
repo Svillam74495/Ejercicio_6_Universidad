5e744fd36b5c46cb5aeddde3b7d07ba360655117
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-27 00:11:30 -0500

    Add files via upload

---
+++ Subsystem0.Curso.java
@@ -0,0 +1,15 @@
+package Subsystem0;
+
+public class Curso {
+
+	private int código;
+
+	private int nombre;
+
+	private int número de créditos;
+
+	private int número de salón;
+
+	private int nombre del edificio;
+
+}
