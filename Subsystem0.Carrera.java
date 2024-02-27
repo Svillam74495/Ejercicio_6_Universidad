a9a6eb16afbc36ea8d13e33951f77b7fc9e17f46
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-27 00:11:03 -0500

    Add files via upload

---
+++ Subsystem0.Carrera.java
@@ -0,0 +1,17 @@
+package Subsystem0;
+
+public class Carrera extends Subsystem0.Facultad {
+
+	private char Nombre;
+
+	private int créditos;
+
+	private int semestres;
+
+	private int nivelcarrera;
+
+	public void operation9() {
+
+	}
+
+}
