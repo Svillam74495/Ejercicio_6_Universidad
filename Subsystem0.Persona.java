8d824db3065926c7d5695a49cd6b5510850e93f6
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-27 00:09:46 -0500

    Add files via upload

---
+++ Subsystem0.Persona.java
@@ -0,0 +1,27 @@
+package Subsystem0;
+
+public class Persona extends Subsystem0.Universidad {
+
+	private char nombre;
+
+	private int cédula;
+
+	private Subsystem0.date fecha;
+
+	private char lugar de nacimiento;
+
+	private char ciudad, departamento, país;
+
+	public void obtener() {
+
+	}
+
+	public void guardar() {
+
+	}
+
+	public void modificar() {
+
+	}
+
+}
---
+++ Subsystem0.Profesor.java
@@ -0,0 +1,23 @@
+package Subsystem0;
+
+public class Profesor extends Subsystem0.Persona {
+
+	private char profesión;
+
+	private int sueldo;
+
+	private char nacionalidad;
+
+	public void obtener() {
+
+	}
+
+	public void guardar() {
+
+	}
+
+	public void modificar() {
+
+	}
+
+}
